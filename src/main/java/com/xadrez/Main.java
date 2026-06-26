ed5b65701628317c7fc490ec1ce2f1effdc00c4d
Jvf76 <joaocaixeta12@unipam.edu.br>
2026-06-26 09:26:58 -0300

    montando o tabuleiro

--- src/main/java/com/xadrez/Main.java
+++ src/main/java/com/xadrez/Main.java
@@ -1,36 +1,55 @@
 package main.java.com.xadrez;
 
-import main.java.com.xadrez.jogo.Cor;
-import main.java.com.xadrez.jogo.Posicao;
+import main.java.com.xadrez.jogo.Peca;
 import main.java.com.xadrez.jogo.Tabuleiro;
-import main.java.com.xadrez.jogo.pecas.Peao;
-import main.java.com.xadrez.jogo.pecas.Torre;
+import main.java.com.xadrez.regras.PartidaXadrez;
 
 public class Main {
     public static void main(String[] args) {
-        Tabuleiro tabuleiro = new Tabuleiro(8, 8);
-
-        Peao peao = new Peao(tabuleiro, Cor.BRANCA);
-        tabuleiro.colocarPeca(peao, new Posicao(4, 3));
-
-        Torre torreAdversaria = new Torre(tabuleiro, Cor.PRETA);
-        tabuleiro.colocarPeca(torreAdversaria, new Posicao(3, 2));
-
-        boolean[][] d = peao.movimentosPossiveis();
+        PartidaXadrez partida = new PartidaXadrez();
+        Tabuleiro tabuleiro = partida.getTabuleiro();
 
         for (int linha = 0; linha < tabuleiro.getLinhas(); linha++) {
             for (int coluna = 0; coluna < tabuleiro.getColunas(); coluna++) {
-                if (linha == 4 && coluna == 3) {
-                    System.out.print("T ");
-                } else if (linha == 3 && coluna == 2) {
-                    System.out.print("A ");
-                } else if (d[linha][coluna]) {
-                    System.out.print("X ");
-                } else {
+                Peca peca = tabuleiro.peca(linha, coluna);
+                if (peca == null) {
                     System.out.print("- ");
+                } else {
+                    System.out.print(letraDaPeca(peca) + " ");
                 }
             }
             System.out.println();
         }
+    }
+
+    private static String letraDaPeca(Peca peca) {
+        String nomeClasse = peca.getClass().getSimpleName();
+        String letra;
+        switch (nomeClasse) {
+            case "Torre":
+                letra = "T";
+                break;
+            case "Cavalo":
+                letra = "C";
+                break;
+            case "Bispo":
+                letra = "B";
+                break;
+            case "Rainha":
+                letra = "Q";
+                break;
+            case "Rei":
+                letra = "R";
+                break;
+            case "Peao":
+                letra = "P";
+                break;
+            default:
+                letra = "?";
+        }
+        if (peca.getCor() == main.java.com.xadrez.jogo.Cor.PRETA) {
+            return letra.toLowerCase();
+        }
+        return letra;
     }
 }
--- src/main/java/com/xadrez/regras/PartidaXadrez.java
+++ src/main/java/com/xadrez/regras/PartidaXadrez.java
@@ -1,4 +1,48 @@
 package main.java.com.xadrez.regras;
 
+import main.java.com.xadrez.jogo.Cor;
+import main.java.com.xadrez.jogo.Posicao;
+import main.java.com.xadrez.jogo.Tabuleiro;
+import main.java.com.xadrez.jogo.pecas.*;
+
 public class PartidaXadrez {
+
+    private Tabuleiro tabuleiro;
+
+    public PartidaXadrez() {
+        tabuleiro = new Tabuleiro(8, 8);
+        configuracaoInicial();
+    }
+
+    private void configuracaoInicial() {
+        // Peças pretas (linhas 0 e 1)
+        tabuleiro.colocarPeca(new Torre(tabuleiro, Cor.PRETA), new Posicao(0, 0));
+        tabuleiro.colocarPeca(new Cavalo(tabuleiro, Cor.PRETA), new Posicao(0, 1));
+        tabuleiro.colocarPeca(new Bispo(tabuleiro, Cor.PRETA), new Posicao(0, 2));
+        tabuleiro.colocarPeca(new Rainha(tabuleiro, Cor.PRETA), new Posicao(0, 3));
+        tabuleiro.colocarPeca(new Rei(tabuleiro, Cor.PRETA), new Posicao(0, 4));
+        tabuleiro.colocarPeca(new Bispo(tabuleiro, Cor.PRETA), new Posicao(0, 5));
+        tabuleiro.colocarPeca(new Cavalo(tabuleiro, Cor.PRETA), new Posicao(0, 6));
+        tabuleiro.colocarPeca(new Torre(tabuleiro, Cor.PRETA), new Posicao(0, 7));
+        for (int coluna = 0; coluna < tabuleiro.getColunas(); coluna++) {
+            tabuleiro.colocarPeca(new Peao(tabuleiro, Cor.PRETA), new Posicao(1, coluna));
+        }
+
+        // Peças brancas (linhas 7 e 6)
+        tabuleiro.colocarPeca(new Torre(tabuleiro, Cor.BRANCA), new Posicao(7, 0));
+        tabuleiro.colocarPeca(new Cavalo(tabuleiro, Cor.BRANCA), new Posicao(7, 1));
+        tabuleiro.colocarPeca(new Bispo(tabuleiro, Cor.BRANCA), new Posicao(7, 2));
+        tabuleiro.colocarPeca(new Rainha(tabuleiro, Cor.BRANCA), new Posicao(7, 3));
+        tabuleiro.colocarPeca(new Rei(tabuleiro, Cor.BRANCA), new Posicao(7, 4));
+        tabuleiro.colocarPeca(new Bispo(tabuleiro, Cor.BRANCA), new Posicao(7, 5));
+        tabuleiro.colocarPeca(new Cavalo(tabuleiro, Cor.BRANCA), new Posicao(7, 6));
+        tabuleiro.colocarPeca(new Torre(tabuleiro, Cor.BRANCA), new Posicao(7, 7));
+        for (int coluna = 0; coluna < tabuleiro.getColunas(); coluna++) {
+            tabuleiro.colocarPeca(new Peao(tabuleiro, Cor.BRANCA), new Posicao(6, coluna));
+        }
+    }
+
+    public Tabuleiro getTabuleiro() {
+        return tabuleiro;
+    }
 }
